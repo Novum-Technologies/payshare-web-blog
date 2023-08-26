85e2690cca4452a6b728355936c57edb84767f88
Aaron Hammer <athammer@buffalo.edu>
2023-08-25 21:58:34 -0600

    wee

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,8 @@ import Layout from '../components/layout'
 import { getAllPostsForHome } from '../lib/api'
 import { CMS_NAME } from '../lib/constants'
 
-export default function Index({ allPosts: { edges }, preview }) {
+export default function Index({ allPosts, preview }) {
+  const edges = allPosts?.edges
   const heroPost = edges?.[0]?.node
   const morePosts = edges?.slice(1) || []
 
